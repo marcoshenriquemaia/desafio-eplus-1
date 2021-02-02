af4fd568a8b31f96711ee68184547ab369479381
marcoshenriquemaia <henriquemarcosmaia@gmail.com>
2021-02-02 09:28:38 -0300

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+const [ array1, array2, array3 ] = require('./mocs')
+
+const filteredArray = array1.removeEquals()
+
+console.log(filteredArray)
---
+++ mocs.js
@@ -0,0 +1,71 @@
+const array1 = [
+  {
+    name: "Marcos",
+    age: 23,
+    address: {
+      street: "Rua jacarezinho",
+      number: 129,
+      cep: "09390-500",
+    },
+  },
+  {
+    name: "João",
+    age: 20,
+    address: {
+      street: "Rua blé",
+      number: 129,
+      cep: "09390-500",
+    },
+  },
+  {
+    name: "Marcos",
+    age: 23,
+    address: {
+      street: "Rua jacarezinho",
+      number: 129,
+      cep: "09390-500",
+    },
+  },
+  {
+    name: "Marcos",
+    age: 25,
+    address: {
+      street: "Rua jacarezinho",
+      number: 129,
+      cep: "09390-500",
+    },
+  },
+  {
+    name: "Maria",
+    age: 18,
+    address: {
+      street: "Rua dos amigos",
+      number: 999,
+      cep: "09390-280",
+    },
+  },
+  {
+    name: "João",
+    age: 20,
+    address: {
+      street: "Rua blé",
+      number: 129,
+      cep: "09390-500",
+    },
+  },
+  {
+    name: "Maria",
+    age: 18,
+    address: {
+      street: "Rua dos amigos",
+      number: 999,
+      cep: "09390-280",
+    },
+  },
+];
+
+const array2 = ["Marcos", "Marcos", "Miguel", "Felipe", "Ana", "Miguel", "Ana", "Felipe"];
+
+const array3 = [0, 1, 6, 5, 6, 3, 8, 7, 7, 1, 0, 2, 9, 66, 4, 10, 1, 11, 20, 10];
+
+module.exports = [array1, array2, array3]
